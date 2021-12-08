75c9c934f2fec3b47f9e77224a0f8d03d58d4f81
Hannah <94702589+hwk01@users.noreply.github.com>
2021-12-07 19:50:01 -0500

    Add files via upload

---
+++ Room.java
@@ -0,0 +1,81 @@
+class Room{
+  Room []directions;
+  private String description;
+  private String environment;
+  private Boolean bossRoom;
+
+  Room(String e){
+    environment = e;
+    if(environment.equals("lava")){
+      description = "The room is filled with lava";
+    }
+    else if(environment.equals("tundra")){
+      description = "The cold rages on in the Tundra";
+    }
+    else if(environment.equals("desert")){
+      description = "Sand surrounds you, no water in sight in the Desert";
+    }
+    else if(environment.equals("swamp")){
+      description = "The swamp smells particularly bad";
+    }
+    else if(environment.equals("clouds")){
+      description = "Up in the air, in the Clouds";
+    }
+    else if(environment.equals("forest")){
+      description = "Trees surround you, its hard to tell where you are headed";
+    }
+    else if(environment.equals("wall")){
+      description = "A wall stands in your way";
+      //Figure out how to auto send backwards
+    }
+    else{
+      description = "Invalid Room Type";
+    }
+    directions = new Room[4];
+    for(int i = 0; i < 4; i++) directions[i] = null;
+  }
+
+  Room(){
+    description = "Blank room";
+    directions = new Room[4];
+    for(int i = 0; i < 4; i++) directions[i] = null;
+  }
+
+  void createRoom(int dir, String enviro){
+    if(directions[dir] != null) return;
+    Room temp = new Room(enviro);
+    directions[dir] = temp;
+    int otherSide = (dir+2)%4;
+    temp.directions[otherSide] = this;
+  }
+
+  void createBossRoom(int dir, String enviro, Boolean boss){
+    if(directions[dir] != null) return;
+    Room temp = new Room(enviro);
+    directions[dir] = temp;
+    int otherSide = (dir+2)%4;
+    temp.directions[otherSide] = this;
+
+    //How to represent the boss is
+
+  }
+
+
+  String getDesc(){
+    return description;
+  }
+
+  String getEnvironment(){
+    return environment;
+  }
+
+  Boolean getBoss(){
+    return bossRoom;
+  }
+
+  @Override
+public String toString() {
+    return description;
+}
+
+}
