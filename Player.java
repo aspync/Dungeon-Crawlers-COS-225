8406ec5e055f0d88e4d2fecfb4ec282fd9b15834
Aspyn Call <94199915+aspync@users.noreply.github.com>
2021-12-15 14:36:47 -0500

    Create Player.java

--- Player.java
+++ Player.java
@@ -1,46 +1,69 @@
 import java.util.Random;
+import java.util.Scanner;
 
-class Player{
+public class Player{ //creates player class
   private int hp;
   private int attack;
   private String name;
 
-  Player(){
+  Player(){ //sets player variables to a default value
     name = "Traveler";
     attack = 1;
     hp = 10;
   }
-  Player(String name, int attack, int hp){
+  Player(String name, int attack, int hp){ //gets name, hp, and attack from user
     this.name = name;
     this.attack = attack;
     this.hp = hp;
   }
-    public boolean takeDamage(int dmg){
-    hp -= dmg;
-    System.out.format("%s takes %d damage\n", name, dmg);
-    if(hp <= 0){
-        System.out.format("%s has died\n", name);
-        return true;
-    }
-    return false;
-    }
-  
-  public String getName(){
+  public String getName(){ //returns the name of the player
     return this.name;
   }
-  public void setName(String name){
+  public void setName(String name){ //sets the player's name to the user's input for name
     if(name.equals(""))
       return;
     this.name = name;
   }
-  public int attack(){
+  public int getAttack(){ //returns the player's attack stat
+    return this.attack;
+  }
+  public int setAttack(Scanner s){ //sets the attack stat to the user's input
+    int input;
+    while(true){
+      try{
+        System.out.println("Your attack can be a number between 1 and 50");
+        input = Integer.parseInt(s.nextLine());
+        break;
+      } catch (NumberFormatException e){
+        System.out.format("\n%s is not a valid input\n", input);
+      }
+    }
+    return input;
+  }
+  public int getHP(){ //returns players hit points
+    return this.hp;
+  }
+  public int setHP(Scanner s){ //sets the player's hit points to the user's input
+    int input;
+    while(true){
+      try{
+        System.out.println("Your hit points can be a number between 1 and 100");
+        input = Integer.parseInt(s.nextLine());
+        break;
+      } catch (NumberFormatException e){
+        System.out.format("\n%s is not a valid input\n", input);
+      }
+    }
+    return input;
+  }
+  public int Attack(){ //generates random number between 1 and the player's attack stat when attacking a monster
     Random rand = new Random();
     return rand.nextInt(attack);
   }
-  public void heal(){
+  public void heal(){ //heals the player for 20 points when called
     this.hp += 20;
   }
-  public boolean isAlive(){
+  public boolean isAlive(){ //keeps track of whether the player is still alive or not
     return hp > 0;
   }
 }
