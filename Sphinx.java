f3db24d578686a3c4dc11e93f2bfa3aaf7e85619
Hannah <94702589+hwk01@users.noreply.github.com>
2021-12-15 14:53:57 -0500

    Update Sphinx.java

--- Sphinx.java
+++ Sphinx.java
@@ -1,71 +1,98 @@
 import java.util.Scanner;
-
+// /equals.ignoreCase?
+// .equalsIgnoreCase()
 class Sphinx extends Entity{
 
   int count = 0;
 
-  public Sphinx(String n,int h,int a){
-    super(n,h,a);
-    n = "Sphinx";
-    h = 10000;
-    a = 10000;
-    System.out.println("In the Desert a large statue is at rest.\nIt has the head of a human, the body of a lion, and the wings of a falcon.\nAs you approach, it comes to life and turns to face you.\n Approach traveler, do not be afraid. Answer these 3 riddles and be on your way.");
-    riddle1();
+  public Sphinx(){
+    super("Sphinx",10000,10000);
+
+
+
   }
 
     public void riddle1(){
-      Scanner scan = new Scanner(System.in);
-      System.out.println("I am something people celebrate or resist. I change people’s thoughts and lives. I am obvious to some people but, to others, I am a mystery. What am I?");
+    System.out.println("\n \nIn the Desert a large statue is at rest.\nIt has the head of a human, the body of a lion, and the wings of a falcon.\nAs you approach, it comes to life and turns to face you.\n Approach traveler, do not be afraid. \n \n Answer these 3 riddles and be on your way.");
 
-      String answer = scan.nextLine();
-      scan.close();
-      if(answer.equals("age") || answer.equals("AGE") || answer.equals("Age")){
-        System.out.println("CORRECT");
-        riddle2();
+      Boolean cor = false;
+
+      while(cor.equals(false)){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\n");
+        System.out.println("I am something people celebrate or resist. I change people’s thoughts and lives. I am obvious to some people but, to others, I am a mystery. What am I?");
+
+        String answer = scan.nextLine();
+        if(answer.equalsIgnoreCase("age")){
+            System.out.println("CORRECT");
+            cor = true;
+            riddle2();
       }
       else{
-        count=+1;
+        count++;
         int chance = 3-count;
+        if(chance == -1){
+          player.takeDamage(101);
+        }
+        System.out.println("\n");
         System.out.format("Try Again, %d chances left",chance);
-
+        System.out.println("\n");
       }
+    }
     }
 
     public void riddle2(){
+      Boolean cor2 = false;
+
+    while(cor2.equals(false)){
       Scanner scan = new Scanner(System.in);
+      System.out.println("\n");
       System.out.println("What grows up while growing down?");
 
       String answer = scan.nextLine();
-      scan.close();
-      if(answer.equals("goose") || answer.equals("Goose") || answer.equals("GOOSE") ||
-      answer.equals("duck") || answer.equals("Duck") || answer.equals("DUCK")){
+      if(answer.equalsIgnoreCase("goose") || answer.equalsIgnoreCase("duck")){
         System.out.println("CORRECT");
+        cor2 = true;
         riddle3();
       }
       else{
-        count=+1;
+        count++;
         int chance = 3-count;
+        if(chance == -1){
+          player.takeDamage(101);
+        }
+        System.out.println("\n");
         System.out.format("Try Again, %d chances left",chance);
-
+        System.out.println("\n");
     }
+  }
   }
 
   public void riddle3(){
+    Boolean cor3 = false;
+
+  while(cor3.equals(false)){
     Scanner scan = new Scanner(System.in);
+    System.out.println("\n");
     System.out.println("What bird do you associate with lifting weight?");
 
     String answer = scan.nextLine();
-    scan.close();
-    if(answer.equals("crane")|| answer.equals("Crane") || answer.equals("CRANE")){
+    if(answer.equalsIgnoreCase("crane")){
       System.out.println("CORRECT");
-      takeDamage(10001);
+      cor3 = true;
+      takeDamage(99999999);
     }
     else{
-      count=+1;
+      count++;
       int chance = 3-count;
+      if(chance == -1){
+        player.takeDamage(101);
+      }
+      System.out.println("\n");
       System.out.format("Try Again, %d chances left",chance);
-
+      System.out.println("\n");
   }
+}
 }
 
 
