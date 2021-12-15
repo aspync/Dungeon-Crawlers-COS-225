f82c6a62feddcc5db26a883566fa92fc1c6f6349
Aspyn Call <94199915+aspync@users.noreply.github.com>
2021-12-15 13:24:44 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,12 +1,12 @@
-class Game{
+import java.util.Random;
+import java.util.Scanner;
 
-  static void doDamage(Entity x){
-    x.takeDamage(10);
-  }
+public class Game{
+
   public static void main(String[]args){
-    Monster a = new Monster("Goblin", 4, 1);
-    Player b = new Player("Tim the Toolman", 900, 100);
-    doDamage(a);
-    doDamage(b);
+    Scanner userInput = new Scanner(System.in);
+    Random rand = new Random();
+
+    
   }
 }
