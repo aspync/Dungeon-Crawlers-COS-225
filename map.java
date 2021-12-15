dbe1bac048e40bc373bef8068dc1e6d73409bc49
Hannah <94702589+hwk01@users.noreply.github.com>
2021-12-15 14:53:12 -0500

    Update map.java

--- map.java
+++ map.java
@@ -3,29 +3,42 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.NumberFormatException;
 
+//create key check in gate Room
+//create walls impassible
+//create End /Winning screen
+//create instakill function for Player
+
+
+
+
+
 class map{
-  static int north = 0;
-  static int east = 1;
-  static int south = 2;
-  static int west = 3;
 
   public static void main(String[] args){
-    Scanner t = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
 
     System.out.println("Welcome to Dungeon Crawler");
-    System.out.println("Select map: map");
+  //  System.out.println("Select map: map");
 
-    String map = t.nextLine() + ".txt";
-    t.close();
-
- Room dungeon[][] = new Room[13][16];
-
-//dungeon [1][3] = new Room;
+//    String map = t.nextLine() + ".txt";
+  //  t.close();
 
 
-    Room start = new Room();
+//start the game;
+    int x = 6;
+    int y = 10;
 
-    File f = new File(map);
+
+
+
+//if(dungeon[x][y]).isWall(){
+//  System.out.print("Cannot enter wall there;")
+//}
+
+
+    Room dungeon[][] = new Room[13][16];
+
+    File f = new File("map.txt");
 
     try{
       int row = 0;
@@ -38,9 +51,10 @@ class map{
   //     System.out.println(temp2);
        for(int i = 0; i < temp2.length; i++){
          dungeon[row][i] = new Room(temp2[i]);
-         System.out.println(dungeon[row][i]);
+    //     System.out.println(dungeon[row][i]);
 
       }
+
          row++;
   }
       s.close();
@@ -52,121 +66,27 @@ class map{
       System.out.println("Bad Input File");
     }
 
+    while(true){
+      //add start at dungeon[7][11];
 
-
-
-
-
-
-
-
-
-/*    start.Room();
-    start.createRoom(north,"tundra");
-    start.createRoom(east,"desert");
-    start.createRoom(west,"tunnel");
-    start.createRoom(south,"clouds");
-
-
-    a = start.directions[east];
-    a.createRoom(north,"swamp");
-    a.createRoom(east,"desert");
-    //a.createRoom(west,tunnel); goes to start
-    a.createRoom(south,"desert");
-
-    b = a.directions[east];
-    b.createRoom(north,"swamp");
-    b.createRoom(east,"desert");
-    //b.createRoom(west,tunnel); goes to A
-    b.createRoom(south,"desert");
-
-
-    c = b.directions[east];
-    c.createRoom(north,"swamp");
-    c.createRoom(east,"wall");
-    //c.createRoom(west,tunnel); goes to B
-    c.createRoom(south,"desert");
-
-    d = c.directions[east];
-    d.createRoom(north,"swamp");
-    d.createRoom(east,"desert");
-    //d.createRoom(west,"tunnel"); goes to C
-    d.createRoom(south,"wall");
-
-    e = d.directions[north];
-    e.createRoom(north,"swamp");
-    e.createRoom(east,"wall");
-    e.createRoom(west,"swamp"); //GOES TO AN ALREADY LINK..PROBLEM?
-    //e.createRoom(south,"clouds"); goes to E
-
-    f = e.directions[north];
-    f.createRoom(north,"swamp");
-    f.createRoom(east,"wall");
-    f.createRoom(west,"wall");
-    //f.createRoom(south,"clouds");
-
-    g = f.directions[north];
-    g.createRoom(north,"swamp");
-    g.createRoom(east,"wall");
-    g.createRoom(west,"swamp");
-  //  g.createRoom(south,"clouds");
-
-    h = g.directions[north];
-    h.createRoom(north,"swamp");
-    h.createRoom(east,"wall");
-    h.createRoom(west,"swamp");
-  //  h.createRoom(south,"clouds");
-
-    i = h.directions[north];
-  //  i.createRoom(north,"tundra");
-    i.createRoom(east,"wall");
-    i.createRoom(west,"swamp");
-  //  i.createRoom(south,"clouds");
-
-//IF NO WALL, DOESNT EXIST, CAN THEY ACCESS
-
-    j = i.directions[west];
-    //j.createRoom(north,"tundra");
-  //  j.createRoom(east,"desert");
-    j.createRoom(west,"wall");
-    j.createRoom(south,"swamp"); // already linked?
-
-
-    k = j.directions[west];
-  //  k.createRoom(north,"tundra");
-  //  k.createRoom(east,"desert");
-    k.createRoom(west,"swamp");
-    k.createRoom(south,"swamp");
-
-*/
-
-// it does not auto link
-//check if wall, check if null
-
-//attempt to read from file
-
-
-
-
-
-
-    //x.east.west = x;
-    //x.east.east = new Room("Also filled with lava");
-    //x.east.east.west = x.east;
-
-  /*  while(true){
-      System.out.println("What direction?");
-      String u = s.nextLine();
-      if(u.equals("east")) {
-        if(x.directions[east] == null) System.out.println("Theres a wall there");
-        else {
-          x = x.directions[east];
-          System.out.println(x.getDesc());
-        }
-      }
+    try{
+      System.out.println(dungeon[x][y].getDesc());
+      System.out.println("Which direction?");
+      String dir = scan.nextLine();
+      if(dir.equalsIgnoreCase("north")) x--;
+      if(dir.equalsIgnoreCase("south")) x++;
+      if(dir.equalsIgnoreCase("east")) y++;
+      if(dir.equalsIgnoreCase("west")) y--;
     }
+    catch (ArrayIndexOutOfBoundsException e){
+      System.out.println("Cannot go that way.");
+      System.out.println("Which direction?");
+      String dir = scan.nextLine();
+    }
+  }
 
-    */
+
+
 
 
   }
