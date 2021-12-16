3b73e02d18d653306a0f9ac7d4c2f55c6c49f57c
Aspyn Call <94199915+aspync@users.noreply.github.com>
2021-12-16 14:12:49 -0500

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Monster extends Entity{
   public Monster(String n, int h, int a){
     super(n, h, a);
-    System.out.println("Monster created");
+    //System.out.println("Monster created");
   }
   public void Battle(Player a, Scanner s){
     while(a.isAlive() && isAlive()){
