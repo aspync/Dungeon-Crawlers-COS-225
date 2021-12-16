dc4272bfe37706a25b7f5254d44b21e2da9d34ed
Hannah <94702589+hwk01@users.noreply.github.com>
2021-12-16 14:27:01 -0500

    Update Sphinx.java

--- Sphinx.java
+++ Sphinx.java
@@ -1,24 +1,20 @@
 import java.util.Scanner;
-// /equals.ignoreCase?
-// .equalsIgnoreCase()
+
+
 class Sphinx extends Entity{
-
-  int count = 0;
-
+  Player a = new Player();
   public Sphinx(){
     super("Sphinx",10000,10000);
-
-
-
   }
 
-    public void riddle1(){
+
+    public static void riddle1(Player a, Monster b, Scanner scan){
+    int count = 0;
     System.out.println("\n \nIn the Desert a large statue is at rest.\nIt has the head of a human, the body of a lion, and the wings of a falcon.\nAs you approach, it comes to life and turns to face you.\n Approach traveler, do not be afraid. \n \n Answer these 3 riddles and be on your way.");
 
       Boolean cor = false;
 
       while(cor.equals(false)){
-        Scanner scan = new Scanner(System.in);
         System.out.println("\n");
         System.out.println("I am something people celebrate or resist. I change people’s thoughts and lives. I am obvious to some people but, to others, I am a mystery. What am I?");
 
@@ -26,13 +22,13 @@ class Sphinx extends Entity{
         if(answer.equalsIgnoreCase("age")){
             System.out.println("CORRECT");
             cor = true;
-            riddle2();
+            riddle2(a,b,scan);
       }
       else{
         count++;
         int chance = 3-count;
         if(chance == -1){
-          player.takeDamage(101);
+          a.takeDamage(1000);
         }
         System.out.println("\n");
         System.out.format("Try Again, %d chances left",chance);
@@ -41,11 +37,11 @@ class Sphinx extends Entity{
     }
     }
 
-    public void riddle2(){
+    public static void riddle2(Player a, Monster b, Scanner scan){
+      int count = 0;
       Boolean cor2 = false;
 
     while(cor2.equals(false)){
-      Scanner scan = new Scanner(System.in);
       System.out.println("\n");
       System.out.println("What grows up while growing down?");
 
@@ -53,13 +49,13 @@ class Sphinx extends Entity{
       if(answer.equalsIgnoreCase("goose") || answer.equalsIgnoreCase("duck")){
         System.out.println("CORRECT");
         cor2 = true;
-        riddle3();
+        riddle3(a,b,scan);
       }
       else{
         count++;
         int chance = 3-count;
         if(chance == -1){
-          player.takeDamage(101);
+          a.takeDamage(1000);
         }
         System.out.println("\n");
         System.out.format("Try Again, %d chances left",chance);
@@ -68,11 +64,11 @@ class Sphinx extends Entity{
   }
   }
 
-  public void riddle3(){
+  public static void riddle3(Player a, Monster b, Scanner scan){
+    int count = 0;
     Boolean cor3 = false;
 
   while(cor3.equals(false)){
-    Scanner scan = new Scanner(System.in);
     System.out.println("\n");
     System.out.println("What bird do you associate with lifting weight?");
 
@@ -80,13 +76,13 @@ class Sphinx extends Entity{
     if(answer.equalsIgnoreCase("crane")){
       System.out.println("CORRECT");
       cor3 = true;
-      takeDamage(99999999);
+      b.takeDamage(99999999);
     }
     else{
       count++;
       int chance = 3-count;
       if(chance == -1){
-        player.takeDamage(101);
+        a.takeDamage(1000);
       }
       System.out.println("\n");
       System.out.format("Try Again, %d chances left",chance);
